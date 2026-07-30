71903079544d80c8e727d3f17e5c648dd352932d
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-07-30 00:38:14 -0400

    Attempts at linting and Generating Docs

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["foundry/**/*", "docs/"]),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      sourceType: "module",
+    },
+    settings: {
+      jsdoc: {
+        mode: "typescript",
+
+        structuredTags: {
+          group: {
+            type: "text",
+          },
+
+          remarks: {
+            type: "text",
+          },
+        },
+      },
+    },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/gfm",
+    extends: ["markdown/recommended"],
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+]);
