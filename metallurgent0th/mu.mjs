fda812c459911e2f283c29b4bfa1dedc4b1f27ac
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-08-15 18:50:19 -0400

    Add HB Helpers

--- metallurgent0th/mu.mjs
+++ metallurgent0th/mu.mjs
@@ -35,6 +35,7 @@ Hooks.once("init", () => {
     fn: (a, b, c) => hitcheckCommand(a, b, c),
   };
 
+  registerHandelbarsHelpers();
   console.log("metallurgent | Initialisation Complete");
 });
 
@@ -54,11 +55,11 @@ async function hitcheckCommand(command, match, chatData) {
   const attacks = await hitcheck(par, qty, burst);
   var message = "<div>";
 
-	if (burst > 1){
-		message += `Par: ${par}, Qty: ${qty}, Burst: ${burst} <br>`
-	} else {
-		message += `Par: ${par}, Qty: ${qty} <br>`
-	}
+  if (burst > 1) {
+    message += `Par: ${par}, Qty: ${qty}, Burst: ${burst} <br>`;
+  } else {
+    message += `Par: ${par}, Qty: ${qty} <br>`;
+  }
 
   attacks.forEach((attack, i, _) => {
     if (attack[0] > 0) {
@@ -121,4 +122,60 @@ async function hitcheck(par = 4, qty = 6, burst = 1) {
     }
   }
   return attacks;
+}
+
+function registerHandelbarsHelpers() {
+  Handlebars.registerHelper("equals", function (v1, v2) {
+    return v1 === v2;
+  });
+
+  Handlebars.registerHelper("contains", function (element, search) {
+    return element.includes(search);
+  });
+
+  Handlebars.registerHelper("concat", function (s1, s2, s3 = "") {
+    return s1 + s2 + s3;
+  });
+
+  Handlebars.registerHelper("isGreater", function (p1, p2) {
+    return p1 > p2;
+  });
+
+  Handlebars.registerHelper("isEqualORGreater", function (p1, p2) {
+    return p1 >= p2;
+  });
+
+  Handlebars.registerHelper("ifOR", function (conditional1, conditional2) {
+    return conditional1 || conditional2;
+  });
+
+  Handlebars.registerHelper("doLog", function (value) {
+    console.log(value);
+  });
+
+  Handlebars.registerHelper("toBoolean", function (string) {
+    return string === "true";
+  });
+
+  Handlebars.registerHelper("for", function (from, to, incr, content) {
+    let result = "";
+
+    for (let i = from; i < to; i += incr) result += content.fn(i);
+
+    return result;
+  });
+
+  Handlebars.registerHelper("times", function (n, content) {
+    let result = "";
+
+    for (let i = 0; i < n; i++) result += content.fn(i);
+
+    return result;
+  });
+
+  Handlebars.registerHelper("notEmpty", function (value) {
+    if (value == 0 || value == "0") return true;
+    if (value == null || value == "") return false;
+    return true;
+  });
 }
