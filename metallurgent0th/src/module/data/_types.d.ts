485a5e0c492cf555e9d14fb7fdff98f41b0b649d
TaeruAlethea <61478538+TaeruAlethea@users.noreply.github.com>
2026-08-19 15:21:38 -0400

    Remove Typescript description files

--- metallurgent0th/src/module/data/_types.d.ts
+++
@@ -1,9 +0,0 @@
-import "./actor/_types";
-import "./item/_types";
-
-export type SubtypeMetadata = {
-  /** The registered document subtype in system.json. */
-  type: string;
-  /** A FontAwesome icon that can be added to `typeIcons`, e.g. `"fa-solid fa-user"`. */
-  icon?: string;
-};
